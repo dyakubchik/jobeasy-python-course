6d057e22675f8131ce3422f97d0c436b9d9d8b2e
dyakubchik <dmitry.yakubchik@gmail.com>
2020-12-12 16:22:52 -0500

    home work_2

--- lesson_2/homework_2_1.py
+++ lesson_2/homework_2_1.py
@@ -1,28 +1,34 @@
 # Create three strings using three different methods. Save your result to result_string_1, result_string_2,
 # result_string_3 variables
 
-result_string_1 = None
-result_string_2 = None
-result_string_3 = None
+result_string_1 = str.capitalize('first_name')
+result_string_2 = str.casefold('LAST_NAME')
+# result_string_3 = str.center(full_name[, fillchar=2])
+# print(result_string_1)
+# print(result_string_2)
+# print(result_string_3)
 
 
 # Enter your first and  last name. Join them together with a space in
 # between. Save a result in a variable result_full_name and
 # save the length of the whole name in result_full_name_length variable.
 
-first_name = None
-last_name = None
-result_full_name = None
-result_full_name_length = None
+# first_name = input("What is your first name:")
+# last_name = input('What is your last name:')
+# result_full_name = first_name + ' ' + last_name
+# result_full_name_length = None
+# print(result_full_name)
 
 
 # Enter the capital city of California State in lower case. Change the case to title case.
 # Save the result in result_ca_capital variable
 
-result_ca_capital = None
+result_ca_capital = "sacramento".capitalize()
+print(result_ca_capital)
 
 
 # Enter the name of our planet. Change the case to upper case. Save the result in
 # result_planet variable
 
-result_planet = None
+result_planet = 'earth'.upper()
+print(result_planet)
